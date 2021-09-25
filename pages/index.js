05ca0db00dc5215c637dec659494124f1a856251
pratham022 <pthombre657@gmail.com>
2021-09-25 16:39:07 +0530

    Implement NextAuth.js

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
 import 'tailwindcss/tailwind.css'
 import Head from 'next/head'
 import Header from '../components/Header'
+import { getSession } from 'next-auth/client'
 
-export default function Home() {
+export default function Home({ session }) {
   return (
     <div>
       <Head>
@@ -18,4 +19,15 @@ export default function Home() {
       </main>
     </div>
   )
+}
+
+export async function getServerSideProps(context) {
+  // get the user
+  const session = await getSession(context);
+
+  return {
+    props: {
+      session
+    }
+  }
 }
