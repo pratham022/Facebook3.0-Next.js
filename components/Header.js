8200d072b9fb73bb25216cc7475364f003732da0
pratham022 <pthombre657@gmail.com>
2021-09-26 15:14:04 +0530

    Add Profile image

--- components/Header.js
+++ components/Header.js
@@ -18,7 +18,10 @@ import {
     ShoppingCartIcon,
 } from '@heroicons/react/outline';
 
+import { useSession, signOut } from 'next-auth/client';
+
 function Header() {
+    const [session] = useSession();
     return (
         <div className="sticky top-0 z-50 bg-white flex items-center p-2 lg:px-5 shadow-md">
 
@@ -52,6 +55,15 @@ function Header() {
             {/* Header Right */}
             <div className="flex items-center sm:space-x-2 justify-end">
                 {/* Profile Pic */}
+                <Image 
+                    onClick={signOut}
+                    className="rounded-full cursor-pointer"
+                    src={session.user.image}
+                    width="40"
+                    height="40"
+                    layout="fixed"
+                />
+
                 <p className="whitespace-nowrap font-semibold pr-3">Prathamesh Thombre</p>
                 <ViewGridIcon className="icon"/>
                 <ChatIcon className="icon"/>
