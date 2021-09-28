e7d491734ad421bfdbd07372115171b0fbe9559a
pratham022 <pthombre657@gmail.com>
2021-09-28 16:39:26 +0530

    Create Story Component

---
+++ components/Feed.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Stories from './Stories'
+
+
+function Feed() {
+    return (
+        <div className="flex-grow h-screen pb-44 pt-6 mr-4
+        xl:mr-40 overflow-y-auto">
+            <div className="mx-auto max-w-md md:max-w-lg lg:max-w-xl">
+                {/* Stories */}
+                <Stories />
+                {/* Input Box */}
+                {/* Posts */}
+
+            </div>
+        </div>
+    )
+}
+
+export default Feed
--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import Header from '../components/Header'
 import { getSession } from 'next-auth/client'
 import Login from '../components/Login'
 import Sidebar from '../components/Sidebar'
+import Feed from '../components/Feed'
 
 export default function Home({ session }) {
   if(!session) return <Login />
@@ -19,6 +20,7 @@ export default function Home({ session }) {
         {/* Sidebar */}
         <Sidebar />
         {/* Feed */}
+        <Feed />
         {/* Widgets */}
       </main>
     </div>
