38b3a1fd8e9a850b32ecd80ddb16e0a895d757cc
pratham022 <pthombre657@gmail.com>
2021-09-30 15:39:25 +0530

    Add image upload feature

--- components/InputBox.js
+++ components/InputBox.js
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
 import Image from 'next/image';
 import { useSession } from 'next-auth/client';
 
@@ -8,6 +8,8 @@ import { CameraIcon, VideoCameraIcon } from "@heroicons/react/solid";
 function InputBox() {
     const [session] = useSession();
     const inputRef = useRef(null);
+    const filePicker = useRef(null);
+    const [imageToPost, setImageToPost] = useState(null);
 
     const sendPost = (e) => {
         e.preventDefault();
@@ -22,6 +24,10 @@ function InputBox() {
             timestamp: new Date().toLocaleString()
         };
 
+        if(imageToPost) {
+            singleData['pic'] = imageToPost
+        }
+
         fetch("http://localhost:8000/posts", {
             method: "POST",
             headers: {
@@ -31,6 +37,23 @@ function InputBox() {
           }).then(
               inputRef.current.value = ""
           );
+          filePicker.current.value = null;
+          setImageToPost(null);
+    }
+
+    const addImageToPost = (e) => {
+        const reader = new FileReader();
+        if(e.target.files[0]) {
+            reader.readAsDataURL(e.target.files[0]);
+        }
+
+        reader.onload = (readerEvent) => {
+            setImageToPost(readerEvent.target.result);
+        }
+    }
+
+    const removeImageFromPost = (e) => {
+        setImageToPost(null);
     }
 
     return (
@@ -54,15 +77,30 @@ function InputBox() {
                         hidden 
                         onClick={sendPost}>Submit</button>
                 </form>
+
+                {imageToPost && (
+                    <div 
+                        onClick={removeImageFromPost} 
+                        className="flex flex-col filter hover:brightness-110 transition duration-150 transform hover:scale-105 cursor-pointer">
+                        
+                        <img src={imageToPost} className="h-10 object-contain"/>
+                        <p className="text-xs text-red-500 text-center">Remove</p>
+                    </div>
+                )}
+
             </div>
             <div className="flex justify-evenly p-3 border-t-2">
                 <div className="inputIcon">
                     <VideoCameraIcon className="h-7 text-red-500"/>
                     <p className="text-xs sm:text-sm xl:text-base">Live Video</p>
                 </div>
-                <div className="inputIcon">
+                <div 
+                    onClick={() => filePicker.current.click()} 
+                    className="inputIcon"
+                >
                     <CameraIcon className="h-7 text-green-400"/>
                     <p className="text-xs sm:text-sm xl:text-base">Photo/Video</p>
+                    <input ref={filePicker} onChange={addImageToPost} type="file" hidden/>
                 </div>
                 <div className="inputIcon">
                     <EmojiHappyIcon className="h-7 text-yellow-300" />
